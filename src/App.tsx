a109e566b958663a9197dae1967e2faa1270f638
saihtun <saihtun1430@gmail.com>
2025-08-02 13:08:27 -0700

    get landing page working

--- src/App.tsx
+++ src/App.tsx
@@ -1,191 +1,261 @@
-import { useState } from "react";
-import {
-  type BackgroundColor,
-  backgroundRemoverService,
-} from "./lib/services/backgroundRemovalService";
+// import { useState } from "react";
+import { Button } from "./components/ui/button";
+import { getSampleImagesAppendHeroText } from "./constants";
 
-interface ProcessingItem {
-  id: string;
-  canvas: HTMLCanvasElement;
-  originalFile: File;
-  progress: number;
-  result?: HTMLCanvasElement;
-  error?: string;
-  status: "pending" | "processing" | "completed" | "error";
+// import {
+//   type BackgroundColor,
+//   backgroundRemoverService,
+// } from "./lib/services/backgroundRemovalService";
+
+// interface ProcessingItem {
+//   id: string;
+//   canvas: HTMLCanvasElement;
+//   originalFile: File;
+//   progress: number;
+//   result?: HTMLCanvasElement;
+//   error?: string;
+//   status: "pending" | "processing" | "completed" | "error";
+// }
+
+function HeroText(props: { key?: string }) {
+  return (
+    <div
+      key={props.key}
+      className="col-span-2 h-[220px] rounded-md bg-neutral-50 flex flex-col items-center justify-center text-2xl space-y-4"
+    >
+      <div>
+        <p>
+          take <span className="font-bold">PhotoID</span> securely--
+        </p>
+        <p>No uploads, No fees, No fuss!</p>
+      </div>
+      <div className="flex gap-4">
+        <Button>Camera</Button>
+        <Button variant={"outline"}>Photo</Button>
+      </div>
+    </div>
+  );
 }
 
 export default function App() {
-  const [items, setItems] = useState<ProcessingItem[]>([]);
-  const [backgroundColor] = useState<BackgroundColor>("white");
-  const [customBgColor] = useState("#ffffff");
-  // const fileInputRef = useRef<HTMLInputElement>(null);
-
-  const fileToCanvas = (file: File): Promise<HTMLCanvasElement> => {
-    return new Promise((resolve, reject) => {
-      const img = new Image();
-      img.src = URL.createObjectURL(file);
-      img.onerror = reject;
-
-      img.onload = () => {
-        const canvas = document.createElement("canvas");
-        canvas.width = img.width;
-        canvas.height = img.height;
-
-        const ctx = canvas.getContext("2d");
-        ctx?.drawImage(img, 0, 0);
-
-        URL.revokeObjectURL(img.src);
-        resolve(canvas);
-      };
-    });
-  };
-
-  const handleFileChange = async (files: FileList) => {
-    const itemPromises = Array.from(files)
-      .filter((file) => file.type.startsWith("image/"))
-      .map(async (file, index) => {
-        const canvas = await fileToCanvas(file);
-        const item: ProcessingItem = {
-          id: `batch_${Date.now()}_${index + 1}`,
-          canvas,
-          originalFile: file,
-          progress: 0,
-          status: "pending",
-        };
-
-        return item;
-      });
-
-    const resolvedItems = await Promise.all(itemPromises);
-    setItems((prev) => [...prev, ...resolvedItems]);
-  };
-
-  const updateItemProgress = (id: string, progress: number) => {
-    setItems((prevItems) => {
-      return prevItems.map((item) =>
-        item.id === id ? { ...item, progress } : item
-      );
-    });
-  };
-
-  const updateItemResult = (id: string, result: HTMLCanvasElement) => {
-    setItems((prevItems) => {
-      return prevItems.map((item) =>
-        item.id === id
-          ? {
-              ...item,
-              result,
-              progress: 100,
-              status: "completed",
-            }
-          : item
-      );
-    });
-  };
-
-  const updateItemError = (id: string, error: string) => {
-    setItems((prevItems) => {
-      return prevItems.map((item) =>
-        item.id === id ? { ...item, error } : item
-      );
-    });
-  };
-
-  const updateItemStatus = (id: string, status: ProcessingItem["status"]) => {
-    setItems((prevItems) => {
-      return prevItems.map((item) =>
-        item.id === id ? { ...item, status } : item
-      );
-    });
-  };
-
-  const processItems = () => {
-    const pendingItems = items.filter((item) => item.status === "pending");
-
-    pendingItems.forEach((item) => {
-      updateItemStatus(item.id, item.status);
-
-      backgroundRemoverService.processImage({
-        canvas: item.canvas,
-        backgroundColor,
-        customBgColor,
-        onProgress: (progress) => updateItemProgress(item.id, progress),
-        onSuccess: (result) => updateItemResult(item.id, result),
-        onError: (error) => updateItemError(item.id, error),
-      });
-    });
-  };
+  const sampleImages = getSampleImagesAppendHeroText();
 
   return (
-    <main>
-      <form>
-        <input
-          type="file"
-          multiple
-          accept="image/*"
-          className="hidden"
-          id="file-upload"
-          onChange={(e) => e.target.files && handleFileChange(e.target.files)}
-        />
-        <label
-          htmlFor="file-upload"
-          style={{
-            cursor: "pointer",
-            padding: "8px 16px",
-            background: "#1976d2",
-            color: "#fff",
-            borderRadius: "4px",
-            display: "inline-block",
-          }}
-        >
-          Upload Images
-        </label>
-      </form>
-      <section className="flex gap-2">
-        {items.length > 0
-          ? items.map((item) => (
-              <canvas
-                key={item.id}
-                ref={(canvas) => {
-                  if (canvas && item.canvas) {
-                    canvas.width = item.canvas.width;
-                    canvas.height = item.canvas.height;
-                    const ctx = canvas.getContext("2d");
-                    if (!ctx) return;
-                    ctx.drawImage(item.canvas, 0, 0);
-                  }
-                }}
-                className="w-40 h-40 border border-gray-300 rounded object-cover"
+    <main className="h-dvh overflow-x-hidden">
+      <nav className="px-6 py-4 shadow-xs">
+        <span className="inline-block font-semibold text-2xl">📸 PhotoID</span>
+      </nav>
+      <section className="max-sm:w-full w-[670px] h-[450px] my-20 m-auto">
+        {/* Desktop */}
+        <div className="md:grid grid-rows-2 grid-cols-3 gap-2 px-2 hidden">
+          {sampleImages.map((src) => {
+            return src === "hero_text" ? (
+              <HeroText key={src} />
+            ) : (
+              <img
+                key={src}
+                src={src}
+                alt="animal img"
+                className="w-[220px] h-[220px] object-cover rounded-md"
               />
-            ))
-          : null}
-      </section>
-      <button
-        type="button"
-        className="p-2 bg-orange-400 rounded-md text-white"
-        onClick={processItems}
-      >
-        RemoveBG
-      </button>
-      <section className="flex gap-2">
-        {items.length > 0
-          ? items.map((item) => (
-              <canvas
-                key={item.id}
-                ref={(canvas) => {
-                  if (canvas && item.result) {
-                    canvas.width = item.result.width;
-                    canvas.height = item.result.height;
-                    const ctx = canvas.getContext("2d");
-                    if (!ctx) return;
-                    ctx.drawImage(item.result, 0, 0);
-                  }
-                }}
-                className="w-40 h-40 border border-gray-300 rounded object-cover"
+            );
+          })}
+        </div>
+
+        {/* Mobile */}
+        <div className="grid grid-cols-1 gap-2 px-2 md:hidden">
+          <HeroText />
+          <div className="flex gap-1">
+            {sampleImages.slice(3).map((src) => (
+              <img
+                key={src}
+                src={src}
+                alt="animal img"
+                className="w-[50%] h-[220px] object-cover rounded-md"
               />
-            ))
-          : null}
+            ))}
+          </div>
+        </div>
       </section>
+      <footer className="text-center w-full fixed bottom-0 left-0 bg-white py-2 shadow font-mono text-sm">
+        <p>Built with ❤️ by Sai, Opensource.</p>
+      </footer>
     </main>
   );
 }
+
+// export function Example() {
+//   const [items, setItems] = useState<ProcessingItem[]>([]);
+//   const [backgroundColor] = useState<BackgroundColor>("white");
+//   const [customBgColor] = useState("#ffffff");
+//   // const fileInputRef = useRef<HTMLInputElement>(null);
+
+//   const fileToCanvas = (file: File): Promise<HTMLCanvasElement> => {
+//     return new Promise((resolve, reject) => {
+//       const img = new Image();
+//       img.src = URL.createObjectURL(file);
+//       img.onerror = reject;
+
+//       img.onload = () => {
+//         const canvas = document.createElement("canvas");
+//         canvas.width = img.width;
+//         canvas.height = img.height;
+
+//         const ctx = canvas.getContext("2d");
+//         ctx?.drawImage(img, 0, 0);
+
+//         URL.revokeObjectURL(img.src);
+//         resolve(canvas);
+//       };
+//     });
+//   };
+
+//   const handleFileChange = async (files: FileList) => {
+//     const itemPromises = Array.from(files)
+//       .filter((file) => file.type.startsWith("image/"))
+//       .map(async (file, index) => {
+//         const canvas = await fileToCanvas(file);
+//         const item: ProcessingItem = {
+//           id: `batch_${Date.now()}_${index + 1}`,
+//           canvas,
+//           originalFile: file,
+//           progress: 0,
+//           status: "pending",
+//         };
+
+//         return item;
+//       });
+
+//     const resolvedItems = await Promise.all(itemPromises);
+//     setItems((prev) => [...prev, ...resolvedItems]);
+//   };
+
+//   const updateItemProgress = (id: string, progress: number) => {
+//     setItems((prevItems) => {
+//       return prevItems.map((item) =>
+//         item.id === id ? { ...item, progress } : item
+//       );
+//     });
+//   };
+
+//   const updateItemResult = (id: string, result: HTMLCanvasElement) => {
+//     setItems((prevItems) => {
+//       return prevItems.map((item) =>
+//         item.id === id
+//           ? {
+//               ...item,
+//               result,
+//               progress: 100,
+//               status: "completed",
+//             }
+//           : item
+//       );
+//     });
+//   };
+
+//   const updateItemError = (id: string, error: string) => {
+//     setItems((prevItems) => {
+//       return prevItems.map((item) =>
+//         item.id === id ? { ...item, error } : item
+//       );
+//     });
+//   };
+
+//   const updateItemStatus = (id: string, status: ProcessingItem["status"]) => {
+//     setItems((prevItems) => {
+//       return prevItems.map((item) =>
+//         item.id === id ? { ...item, status } : item
+//       );
+//     });
+//   };
+
+//   const processItems = () => {
+//     const pendingItems = items.filter((item) => item.status === "pending");
+
+//     pendingItems.forEach((item) => {
+//       updateItemStatus(item.id, item.status);
+
+//       backgroundRemoverService.processImage({
+//         canvas: item.canvas,
+//         backgroundColor,
+//         customBgColor,
+//         onProgress: (progress) => updateItemProgress(item.id, progress),
+//         onSuccess: (result) => updateItemResult(item.id, result),
+//         onError: (error) => updateItemError(item.id, error),
+//       });
+//     });
+//   };
+
+//   return (
+//     <main>
+//       <form>
+//         <input
+//           type="file"
+//           multiple
+//           accept="image/*"
+//           className="hidden"
+//           id="file-upload"
+//           onChange={(e) => e.target.files && handleFileChange(e.target.files)}
+//         />
+//         <label
+//           htmlFor="file-upload"
+//           style={{
+//             cursor: "pointer",
+//             padding: "8px 16px",
+//             background: "#1976d2",
+//             color: "#fff",
+//             borderRadius: "4px",
+//             display: "inline-block",
+//           }}
+//         >
+//           Upload Images
+//         </label>
+//       </form>
+//       <section className="flex gap-2">
+//         {items.length > 0
+//           ? items.map((item) => (
+//               <canvas
+//                 key={item.id}
+//                 ref={(canvas) => {
+//                   if (canvas && item.canvas) {
+//                     canvas.width = item.canvas.width;
+//                     canvas.height = item.canvas.height;
+//                     const ctx = canvas.getContext("2d");
+//                     if (!ctx) return;
+//                     ctx.drawImage(item.canvas, 0, 0);
+//                   }
+//                 }}
+//                 className="w-40 h-40 border border-gray-300 rounded object-cover"
+//               />
+//             ))
+//           : null}
+//       </section>
+//       <button
+//         type="button"
+//         className="p-2 bg-orange-400 rounded-md text-white"
+//         onClick={processItems}
+//       >
+//         RemoveBG
+//       </button>
+//       <section className="flex gap-2">
+//         {items.length > 0
+//           ? items.map((item) => (
+//               <canvas
+//                 key={item.id}
+//                 ref={(canvas) => {
+//                   if (canvas && item.result) {
+//                     canvas.width = item.result.width;
+//                     canvas.height = item.result.height;
+//                     const ctx = canvas.getContext("2d");
+//                     if (!ctx) return;
+//                     ctx.drawImage(item.result, 0, 0);
+//                   }
+//                 }}
+//                 className="w-40 h-40 border border-gray-300 rounded object-cover"
+//               />
+//             ))
+//           : null}
+//       </section>
+//     </main>
+//   );
+// }
---
+++ src/constants/index.ts
@@ -0,0 +1,14 @@
+const sampleImages = [
+  "https://plus.unsplash.com/premium_photo-1722859221349-26353eae4744?q=80&w=1336&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?q=80&w=1317&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1518717758536-85ae29035b6d?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1543466835-00a7907e9de1?q=80&w=2148&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1671360770124-fe42927790e2?q=80&w=1288&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1574144611937-0df059b5ef3e?q=80&w=1364&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+];
+
+export function getSampleImagesAppendHeroText() {
+  const modSampleImages = sampleImages.slice(0, 4);
+  modSampleImages.splice(1, 0, "hero_text");
+  return modSampleImages;
+}
